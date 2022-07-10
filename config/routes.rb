92af23133c7aa788cb7d9ba72d13e478bf19ec8c
Diptiman <diptiman.pati@byjus.com>
2022-07-10 23:51:47 +0530

    1st commit of chap 8

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
   get 'users/new'
-   root 'static_pages#home'
+  root 'static_pages#home'
 
    #get 'static_pages/home'
 
@@ -15,6 +16,10 @@ Rails.application.routes.draw do
    get 'about'   => 'static_pages#about'
    get 'contact' => 'static_pages#contact'
    get 'signup'  => 'users#new'
+
+   get    'login'   => 'sessions#new'
+   post   'login'   => 'sessions#create'
+   delete 'logout'  => 'sessions#destroy'
 
    resources :users
 end
