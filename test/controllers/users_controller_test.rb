24952a0f72b288ca2da4b9a587d2aba3b1a05cde
Diptiman <diptiman.pati@byjus.com>
2022-07-14 01:52:02 +0530

    4th commit of chap - 9

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -42,4 +42,19 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   #   assert_redirected_to root_url
   # end
   #Not Clear
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete '/users/:id', params: {id: @user}
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete '/users/:id', params: {id: @user}
+    end
+    assert_redirected_to root_url
+  end
 end
