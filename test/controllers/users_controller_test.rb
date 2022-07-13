3c4a35fbfea3188248f865164873fe4c75fb99b5
Diptiman <diptiman.pati@byjus.com>
2022-07-13 14:47:39 +0530

    1st commit of chap-9

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,10 +1,27 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
   test "should get new" do
     # get users_new_url
-    get signup_url
+    get '/signup'
+    # get signup_url
+    
     assert_response :success
   end
+  test "should redirect edit when not logged in" do
+    #get '/users/:id/edit', params: {id: @user}
+    get edit_user_url(@user), params: {id: @user}
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+  test "should redirect update when not logged in" do
+    patch '/users/:id', params: {id: @user, user: { name: @user.name, email: @user.email }}
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+  
 
 end
