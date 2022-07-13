7936851cf4117bd909976b62ae9478a08f18c35b
Diptiman <diptiman.pati@byjus.com>
2022-07-13 23:25:44 +0530

    2nd commit of chap - 9

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,6 +3,11 @@ require 'test_helper'
 class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
+    @other_user = users(:archer)
+  end
+  test "should redirect index when not logged in" do
+    get users_url
+    assert_redirected_to login_url
   end
   test "should get new" do
     # get users_new_url
@@ -22,6 +27,19 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     assert_redirected_to login_url
   end
-  
+  test "should redirect edit when logged in as wrong user" do
+    log_in_as(@other_user)
+    get edit_user_url(@user), params: {id: @user}
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
 
+  # Not Clear............
+  # test "should redirect update when logged in as wrong user" do
+  #   log_in_as(@other_user)
+  #   patch '/users/:id', params: {id: @user, user: { name: @user.name, email: @user.email }}
+  #   assert flash.empty?
+  #   assert_redirected_to root_url
+  # end
+  #Not Clear
 end
