f47b39a8c666dd8faf9655f61fa757140a73fdd6
Diptiman <diptiman.pati@byjus.com>
2022-08-10 18:18:24 +0530

    before graphql

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,15 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
+#===================
+  def group
+      
+  end
 
+  def adduserstothegrp
+    
+  end
+#====================
   def index
     @users = User.paginate(page: params[:page])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get 'group/index'
   get 'password_resets/new'
   get 'password_resets/edit'
   get 'sessions/new'
@@ -19,17 +20,23 @@ Rails.application.routes.draw do
    get 'contact' => 'static_pages#contact'
    get 'signup'  => 'users#new'
 
-   get    'login'   => 'sessions#new'
-   post   'login'   => 'sessions#create'
-   delete 'logout'  => 'sessions#destroy'
-    
+    get    'login'   => 'sessions#new'
+    post   'login'   => 'sessions#create'
+    delete 'logout'  => 'sessions#destroy'
+
+    get 'group' => 'users#group'
+
+
     resources :users do
       member do
         get :following, :followers
+        post :adduserstothegrp
       end
     end
    resources :account_activations, only: [:edit]
    resources :password_resets,     only: [:new, :create, :edit, :update]
    resources :microposts,          only: [:create, :destroy]
    resources :relationships,       only: [:create, :destroy]
+
+   
 end
